1e15f61ae1ead68232680b30f638e1344270078e
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-11-07 16:53:38 -0600

    Added qr-code generate

--- README.md
+++ README.md
@@ -6,7 +6,6 @@
 
 ![basics](https://user-images.githubusercontent.com/4677417/186188965-73453154-fdec-4d6b-9c34-cb35c248ae5b.png)
 
-
 ## 🚀 Project Structure
 
 Inside of your Astro project, you'll see the following folders and files:
@@ -47,3 +46,7 @@ All commands are run from the root of the project, from a terminal:
 ## 👀 Want to learn more?
 
 Feel free to check [our documentation](https://docs.astro.build) or jump into our [Discord server](https://astro.build/chat).
+
+# Ejecutar en dev cloudflare
+
+npx wrangler pages dev ./dist
---
+++ functions/api/generate-qr.js
@@ -0,0 +1,31 @@
+
+import qr from "qrcode";
+
+export async function onRequestPost(context) {
+  // Contents of context object
+  // const {
+  //   request, // same as existing Worker API
+  //   env, // same as existing Worker API
+  //   params, // if filename includes [id] or [[path]]
+  //   waitUntil, // same as ctx.waitUntil in existing Worker API
+  //   passThroughOnException, // same as ctx.passThroughOnException in existing Worker API
+  //   next, // used for middleware or to fetch assets
+  //   data, // arbitrary space for passing data between middlewares
+  // } = context;
+
+  const { request } = context;
+  const { url } = await request.json();
+
+  const qrImage = await qr.toString(url, {
+    type: 'svg', color: {
+      light: '#3685FF',
+      dark: '#fff'
+    }
+  });
+
+  return new Response(JSON.stringify({ svg: qrImage }), {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }); // Web API
+}
